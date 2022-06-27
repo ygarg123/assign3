63f6bb6a32f17ba2e8f816bbdaee85dfb1737d25
Yatin <yatingarg378@gmail.com>
2022-06-27 11:35:43 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject, Observable, of } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'assign3';
+export class AppComponent implements OnInit {
+  inputs$ = new Subject<any>();
+  filterKey: any;
+  items: any[] = [];
+  filteredItems: any;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.filterKey = '';
+    this.items = [
+      'apple',
+      'banana',
+      'orange',
+      'pineapple',
+      'grapes',
+      'mango',
+      'cherry',
+      'blackberry',
+      'strawberry',
+      'peach',
+      'guava',
+      'papaya',
+    ];
+    this.filteredItems = this.items;
+
+    this.inputs$
+      .pipe(
+        debounceTime(250),
+        distinctUntilChanged(),
+        switchMap((input) => {
+          let key = input.trim();
+          let result = this.items.filter((item: string) =>
+            item.toLowerCase().includes(key.toLowerCase())
+          );
+
+          return of(result);
+        })
+      )
+      .subscribe((result) => {
+        this.filteredItems = result;
+      });
+  }
+
+  onFilterKeyChange(event: any) {
+    this.filterKey = event.target.value;
+    this.inputs$.next(this.filterKey);
+  }
 }
